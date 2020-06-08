a641c79a58102f3668b07d4f75d70ec5ef3d5604
shhqq <shhicc@163.com>
2020-06-08 10:34:08 +0800

    将基于xml的示例的test方法改为spring提供的main方法

--- day03_SpringAOP_01_Demo/src/test/java/com/springLearn/test/AccountServiceTest.java
+++ day03_SpringAOP_01_Demo/src/test/java/com/springLearn/test/AccountServiceTest.java
@@ -7,24 +7,34 @@ package com.springLearn.test;
 import com.springLearn.domain.Account;
 import com.springLearn.service.IAccountService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
 /**
  * 使用junit单元测试，测试我们的配置
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:bean.xml")
 public class AccountServiceTest {
+
+    @Autowired
+    private IAccountService as;
+
     /**
      * 右键自动生成testMethod
      */
     @Test
     public void testFindAll() {
-        // 1. 获取spring容器
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-        // 2. 得到业务层对象
-        IAccountService as = (IAccountService) ac.getBean("accountService");
+//        // 1. 获取spring容器
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+//        // 2. 得到业务层对象
+//        IAccountService as = (IAccountService) ac.getBean("accountService");
         // 3. 执行方法，sql
         List<Account> accounts = as.findAllAccount();
         // 4. 遍历结果
@@ -35,8 +45,8 @@ public class AccountServiceTest {
 
     @Test
     public void testFindOne() {
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-        IAccountService as = (IAccountService) ac.getBean("accountService");
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+//        IAccountService as = (IAccountService) ac.getBean("accountService");
         Account account = as.findAccountById(1);
         System.out.println(account);
     }
@@ -44,8 +54,8 @@ public class AccountServiceTest {
     @Test
     public void testSave() {
 
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-        IAccountService as = (IAccountService) ac.getBean("accountService");
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+//        IAccountService as = (IAccountService) ac.getBean("accountService");
         // 新建Account对象
         Account account = new Account();
         account.setName("ddd");
@@ -57,8 +67,8 @@ public class AccountServiceTest {
 
     @Test
     public void testUpdate() {
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-        IAccountService as = (IAccountService) ac.getBean("accountService");
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+//        IAccountService as = (IAccountService) ac.getBean("accountService");
 
         Account account = as.findAccountById(1);
         account.setMoney(3000F);
@@ -68,8 +78,8 @@ public class AccountServiceTest {
 
     @Test
     public void testDelete() {
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-        IAccountService as = (IAccountService) ac.getBean("accountService");
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+//        IAccountService as = (IAccountService) ac.getBean("accountService");
         as.deleteAccount(4);
     }
 }
