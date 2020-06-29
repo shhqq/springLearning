4464a23835a4473dd9c0adafbef502f8960b190e
shhqq <shhicc@163.com>
2020-06-29 10:19:18 +0800

    day03--ssm整合--step4--搭建并测试springMVC框架

--- springMVCLearn/day03_ssmIntegrated_01_integratedSSM/src/main/java/com/ssm/controller/AccountController.java
+++ springMVCLearn/day03_ssmIntegrated_01_integratedSSM/src/main/java/com/ssm/controller/AccountController.java
@@ -5,12 +5,22 @@ package com.ssm.controller;
  */
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 表现层
  */
 @Controller
+@RequestMapping("/account")
 public class AccountController {
 
-
+    /**
+     * 表现层方法
+     * @return 页面跳转
+     */
+    @RequestMapping("testFindAll")
+    public String testFindAll(){
+        System.out.println("表现层testFindAll方法");
+        return "success";
+    }
 }
--- springMVCLearn/day03_ssmIntegrated_01_integratedSSM/src/main/webapp/WEB-INF/pages/success.jsp
+++ springMVCLearn/day03_ssmIntegrated_01_integratedSSM/src/main/webapp/WEB-INF/pages/success.jsp
@@ -2,15 +2,14 @@
   Created by IntelliJ IDEA.
   User: pc
   Date: 2020/6/29
-  Time: 10:10
+  Time: 10:12
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>HomePage</title>
+    <title>Success</title>
 </head>
 <body>
-<h3>springMVC测试</h3>
-<a href="account/testFindAll">springMVC测试</a>
+<h3>执行成功</h3>
 </body>
 </html>
