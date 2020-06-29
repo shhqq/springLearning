a771c02bbf4fcd20bcfaa48e4e9a4d9a30cefe70
shhqq <shhicc@163.com>
2020-06-29 14:46:52 +0800

    day03--ssm整合--step8--添加spring声明式事务控制

--- springMVCLearn/day03_ssmIntegrated_01_integratedSSM/src/main/java/com/ssm/controller/AccountController.java
+++ springMVCLearn/day03_ssmIntegrated_01_integratedSSM/src/main/java/com/ssm/controller/AccountController.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -38,5 +41,24 @@ public class AccountController {
         // 将结果添加到Model对象中，在jsp中可以显示数据
         model.addAttribute("accounts", accounts);
         return "success";
+    }
+
+    /**
+     * 测试保存账户方法，需要添加事务控制。
+     * 保存账户后，重定向到查询所有账户方法。
+     * @param account account to save.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws IOException
+     */
+    @RequestMapping("/testSaveAccount")
+    public void testSaveAccount(Account account, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("表现层的testSaveAccount方法");
+
+        // 执行保存方法
+        accountService.saveAccount(account);
+        // 页面重定向到查询所有账户
+        response.sendRedirect(request.getContextPath() + "/account/testFindAll");
+        return;
     }
 }
--- springMVCLearn/day03_ssmIntegrated_01_integratedSSM/src/main/webapp/index.jsp
+++ springMVCLearn/day03_ssmIntegrated_01_integratedSSM/src/main/webapp/index.jsp
@@ -12,5 +12,12 @@
 <body>
 <h3>springMVC测试</h3>
 <a href="account/testFindAll">springMVC测试</a>
+
+<h3>Mybatis测试，保存账户</h3>
+<form action="account/testSaveAccount" method="post"/><br/>
+    <label>name</label><input type="text" name="name"/><br/>
+    <label>money</label><input type="text" name="money"/><br/>
+    <input type="submit" value="save"/><br/>
+</form>
 </body>
 </html>
