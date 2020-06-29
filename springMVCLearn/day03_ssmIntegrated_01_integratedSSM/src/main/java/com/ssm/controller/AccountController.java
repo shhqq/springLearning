8ba67b9b2fb70fb471bb7cf188b5220f2dbb248b
shhqq <shhicc@163.com>
2020-06-29 10:54:30 +0800

    day03--ssm整合--step5--Spring整合SpringMVC框架

--- springMVCLearn/day03_ssmIntegrated_01_integratedSSM/src/main/java/com/ssm/controller/AccountController.java
+++ springMVCLearn/day03_ssmIntegrated_01_integratedSSM/src/main/java/com/ssm/controller/AccountController.java
@@ -4,6 +4,8 @@ package com.ssm.controller;
  * Created by s on 2020/6/29 09:27.
  */
 
+import com.ssm.service.IAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,12 +17,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AccountController {
 
     /**
+     * 提供业务层对象，并使用Autowired注解自动按类型注入
+     */
+    @Autowired
+    private IAccountService accountService;
+
+    /**
      * 表现层方法
      * @return 页面跳转
      */
     @RequestMapping("testFindAll")
     public String testFindAll(){
         System.out.println("表现层testFindAll方法");
+        // 整合：调用业务层方法，就实现了表现层调用业务层方法
+        accountService.findAll();
         return "success";
     }
 }
