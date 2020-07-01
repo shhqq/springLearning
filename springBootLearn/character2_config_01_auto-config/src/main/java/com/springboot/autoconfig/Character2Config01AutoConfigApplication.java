9a544c792cf780fd598df4c17136abb454912724
shhqq <shhicc@163.com>
2020-07-01 16:53:29 +0800

    character2--配置文件--自动配置原理--创建新项目

---
+++ springBootLearn/character2_config_01_auto-config/src/main/java/com/springboot/autoconfig/Character2Config01AutoConfigApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.autoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Character2Config01AutoConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Character2Config01AutoConfigApplication.class, args);
+    }
+
+}
