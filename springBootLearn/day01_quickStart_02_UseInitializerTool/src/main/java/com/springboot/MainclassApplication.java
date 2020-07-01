0fdd22e722f2060d19a74389dabdd57aead8c294
shhqq <shhicc@163.com>
2020-07-01 10:43:55 +0800

    character2--springBoot配置文件--使用PropertiesSource，ImportResource

--- springBootLearn/day01_quickStart_02_UseInitializerTool/src/main/java/com/springboot/MainclassApplication.java
+++ springBootLearn/day01_quickStart_02_UseInitializerTool/src/main/java/com/springboot/MainclassApplication.java
@@ -2,8 +2,10 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(value = "classpath:bean.xml")
 public class MainclassApplication {
 
     public static void main(String[] args) {
--- springBootLearn/day01_quickStart_02_UseInitializerTool/src/test/java/com/springboot/mainclass/MainclassApplicationTests.java
+++ springBootLearn/day01_quickStart_02_UseInitializerTool/src/test/java/com/springboot/mainclass/MainclassApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)    // 自动生成的项目需要添加，并添加junit依赖坐标
@@ -13,10 +15,22 @@ class MainclassApplicationTests {
     @Autowired
     Person person;
 
+    @Autowired
+    ApplicationContext ac;
+//    ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+
     @Test
     void contextLoads() {
         System.out.println("测试ConfigurationProperties");
         System.out.println(person);
     }
 
+    /**
+     * 测试配置spring的配置文件bean.xml
+     */
+    @Test
+    void testBeanXml(){
+        boolean b = ac.containsBean("BeanPerson");
+        System.out.println(b);
+    }
 }
