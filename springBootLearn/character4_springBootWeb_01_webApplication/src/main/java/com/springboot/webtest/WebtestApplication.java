2d946edd8a5c451fba7f7fa8410dccdd3f4f0757
shhqq <shhicc@163.com>
2020-07-03 17:33:51 +0800

    character4--web开发--添加一个ViewResolver实现类，测试MVC自动配置原理

--- springBootLearn/character4_springBootWeb_01_webApplication/src/main/java/com/springboot/webtest/WebtestApplication.java
+++ springBootLearn/character4_springBootWeb_01_webApplication/src/main/java/com/springboot/webtest/WebtestApplication.java
@@ -2,6 +2,11 @@ package com.springboot.webtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class WebtestApplication {
@@ -10,4 +15,21 @@ public class WebtestApplication {
         SpringApplication.run(WebtestApplication.class, args);
     }
 
+    /**
+     * 编写一个ViewResolver实现类，提供一个方法，将对象加入容器
+     * 在DispatcherServlet.java中的doDispatch()方法加一个断点，debug下就可以看到自定义的ViewResolver对象
+     * @return
+     */
+    @Bean
+    public ViewResolver myViewResolver(){
+        return new myViewResolver();
+    }
+
+    public static class myViewResolver implements ViewResolver{
+
+        @Override
+        public View resolveViewName(String s, Locale locale) throws Exception {
+            return null;
+        }
+    }
 }
